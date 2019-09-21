36583b0c1a94101bdc6e5bdc8f5b9e4f682ff355
VVcattie <48469990+VVcattie@users.noreply.github.com>
2019-09-21 16:00:54 -0600

    Update lab3_script.sh

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,6 +4,8 @@
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
+#I use echo and read to start, then use the egreg to search everything we want. Because we don't need the exact phone number and emails so I used -c
+#I catch the gercities.com from regex_practice.txt to a new file by grep and >>
 echo "enter filename"
 read filename
 echo "enter regex"
